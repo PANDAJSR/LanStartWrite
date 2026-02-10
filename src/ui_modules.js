d4e4094457d0b74adda2192b8897c57aa67e7711
Fox_block <system_linux_cmb@163.com>
2026-02-10 13:26:18 +0800

    删库

--- src/ui_modules.js
+++
@@ -1,5 +0,0 @@
-// ui_modules.js - centralized exports for UI-related modules
-export { default as Pen } from './tool_bar/pen.js';
-export { default as Erase } from './tool_bar/erese.js';
-export * from './tool_bar/more_decide_windows.js';
-export * from './renderer.js';
